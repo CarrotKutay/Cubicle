09a943e693e9fca78f74b3627d0f19e30b33813b
unknown <s0564026@htw-berlin.de>
2019-10-28 14:06:10 +0100

    Added simple UI

---
+++ Assets/Scripts/UI.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UI : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void onClickPlay()
+    {
+        SceneManager.LoadScene("MistyMountains");
+
+    }
+    public void onClickSettings()
+    {
+        SceneManager.LoadScene("MistyMountains");
+    }
+
+    public void onClickControl()
+    {
+        SceneManager.LoadScene("ControlScene");
+    }
+
+    public void onClickQuit()
+    {
+        Application.Quit();
+    }
+
+    public void onClickBackToMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
+
