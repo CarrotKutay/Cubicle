8311c4fd3ffc5cdfa0c2c11d04be7d9775b4b6ae
unknown <s0564026@htw-berlin.de>
2019-11-04 16:40:45 +0100

    Script for Tommys Stage

---
+++ Assets/Scripts/StageTommy.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageTommy : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject leftPlatform;
+    [SerializeField]
+    private GameObject rightPlatform;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        leftPlatform.transform.Rotate(0, 0, 60 * Time.deltaTime);
+
+        rightPlatform.transform.position = new Vector3(rightPlatform.transform.position.x, 2 * Time.time, rightPlatform.transform.position.z);
+
+        if (rightPlatform.transform.position.y > 3.0f)
+        {
+            
+        }
+    }
+}
