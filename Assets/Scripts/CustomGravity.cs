4c244cff9e856020b02116fa850b4bf3be6bc8d6
unknown <s0564026@htw-berlin.de>
2019-11-04 13:04:23 +0100

    Added the Player Movement

---
+++ Assets/Scripts/CustomGravity.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomGravity : MonoBehaviour
+{
+
+    [SerializeField]
+    float gravityScale = 3.0f;
+
+    private static float globalGravity = -9.81f;
+
+    private Rigidbody rb;
+
+    void OnEnable()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.useGravity = false;
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 gravity = globalGravity * gravityScale * Vector3.up;
+        rb.AddForce(gravity, ForceMode.Acceleration);
+    }
+}
+
