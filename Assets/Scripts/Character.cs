bfbb5ee56f5711745d606b7a177c964caaa2a4a3
ammon <s0566075@htw-berlin.de>
2019-11-25 10:22:11 +0100

    [#25] added working two weapon slot system

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -44,13 +44,21 @@ public class Character : MonoBehaviour
         {
             Weapon.transform.parent = WeaponSlot1.Transform;
             WeaponSlot1.HoldsWeapon = true;
-            if (!WeaponSlot2.HoldsWeapon) { WeaponSlot1.IsActiveSlot = true; }
+            if (!WeaponSlot2.HoldsWeapon)
+            {
+                WeaponSlot1.IsActiveSlot = true;
+                WeaponSlot2.IsActiveSlot = false;
+            }
         }
         else if (!WeaponSlot2.HoldsWeapon) // equip weapon on slot 2 if empty
         {
             Weapon.transform.parent = WeaponSlot2.Transform; ;
             WeaponSlot2.HoldsWeapon = true;
-            if (!WeaponSlot1.HoldsWeapon) { WeaponSlot2.IsActiveSlot = true; }
+            if (!WeaponSlot1.HoldsWeapon)
+            {
+                WeaponSlot2.IsActiveSlot = true;
+                WeaponSlot1.IsActiveSlot = false;
+            }
         }
         Weapon.transform.localPosition = Vector3.zero;
         Weapon.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezePosition;
@@ -91,9 +99,18 @@ public class Character : MonoBehaviour
             Transform dropWeapon = weaponSlot.Transform.GetChild(0);
             dropWeapon.transform.parent = null;
             dropWeapon.transform.position = new Vector3(transform.position.x, transform.position.y, 0);
-            weaponSlot.IsActiveSlot = false;
             weaponSlot.Transform.DetachChildren();
             weaponSlot.HoldsWeapon = false;
+        }
+        weaponSlot = WeaponSlot1.IsActiveSlot ? WeaponSlot2 : WeaponSlot1;
+        if (weaponSlot.HoldsWeapon)
+        {
+            changeActiveWeapon();
+        }
+        else
+        {
+            WeaponSlot1.IsActiveSlot = false;
+            WeaponSlot2.IsActiveSlot = false;
         }
     }
 
@@ -176,8 +193,12 @@ public class Character : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (!checkingHealth) StartCoroutine(healthcare());
+        if (!checkingHealth)
+        {
+            StartCoroutine(healthcare());
+        }
         checkReload();
+        checkActiveWeaponSwap();
     }
 
     private void OnCollisionStay(Collision other)
@@ -191,6 +212,13 @@ public class Character : MonoBehaviour
         }
     }
 
+    private void checkActiveWeaponSwap()
+    {
+        if (Input.GetButtonDown("Swap Weapon"))
+        {
+            changeActiveWeapon();
+        }
+    }
     private void checkReload()
     {
         if (Input.GetButtonDown("Reload"))
